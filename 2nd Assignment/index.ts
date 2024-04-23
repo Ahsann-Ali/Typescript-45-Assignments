7a97255d8ca8eae4a0669196024add9aa46644e8
ahsann-alii <ahsanzafar165@gmail.com>
2024-04-23 20:55:20 +0500

    first commit

---
+++ 2nd Assignment/index.ts
@@ -0,0 +1,6 @@
+// Assignment No#2 
+// Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let person_name: string = "Ahsan";
+console.log(`Hello ${person_name}, would you like to earn some dollors today?`);
---
+++ 3rd Assignment/index.ts
@@ -0,0 +1,17 @@
+// Assignment #3 
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+// storing a person name in a veriable
+let person_name : string = "Ahsan ali";
+
+// print a person name in lowerCase
+
+console.log("Lower Case:", person_name.toLowerCase());
+
+// print a person name in upperCase
+
+console.log("Upper Case:", person_name.toUpperCase());
+
+// print a person name in titleCase
+
+console.log("Title Case:", person_name.charAt(0).toUpperCase() + person_name.slice(1,6).toLowerCase() + person_name.charAt(6).toUpperCase() + person_name.slice(7).toLowerCase());
