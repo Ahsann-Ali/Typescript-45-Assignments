2af1a15bad921bae4881c9f2b84301f80ead6b9d
Ahsann-Ali <saltyswag372@gmail.com>
2024-04-24 14:08:11 +0500

    first commit

---
+++ 4th Assignment/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Assignment# 4
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Storing a famous quote and its author name in veriable
+let quote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Einstein";
+//lets print
+console.log(`${author} once said,"${quote}"`);
